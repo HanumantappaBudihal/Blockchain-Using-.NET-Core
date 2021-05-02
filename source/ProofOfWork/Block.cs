0f57cd056cbff397b839782a164008702610c46b
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2021-05-03 01:52:32 +0530

    ProofOfWork : Basic structure implementaion for the chain

---
+++ source/ProofOfWork/Block.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ProofOfWork
+{
+    public class Block
+    {
+        public int Index { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string PreviousHash { get; set; }
+        public string Hash { get; set; }
+        public string Data { get; set; }
+
+        public Block(DateTime timeStamp, string previousHash, string data)
+        {
+            TimeStamp = timeStamp;
+            PreviousHash = previousHash;
+            Data = data;
+            Hash = GenerateHash();
+        }
+
+        public string GenerateHash()
+        {
+            SHA256 sha256 = SHA256.Create();
+
+            byte[] inputBytes = Encoding.ASCII.GetBytes($"{TimeStamp}-{PreviousHash ?? ""}-{Data}");
+            byte[] outputBytes = sha256.ComputeHash(inputBytes);
+
+            return Convert.ToBase64String(outputBytes);
+        }
+    }
+}
---
+++ source/ProofOfWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProofOfWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
