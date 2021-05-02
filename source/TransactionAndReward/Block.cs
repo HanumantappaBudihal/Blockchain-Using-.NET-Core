ded84c21b162f33174414ffb9d7cf075303656a1
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2021-05-03 02:23:43 +0530

    Transaction & Rewards : Added the initial structure and classes

---
+++ source/TransactionAndReward/Block.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TransactionAndReward
+{
+    public class Block
+    {
+        public int Index { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string PreviousHash { get; set; }
+        public string Hash { get; set; }
+        public string Data { get; set; }
+        public int Nouce { get; set; }
+
+        public Block(DateTime timeStamp, string previousHash, string data)
+        {
+            TimeStamp = timeStamp;
+            PreviousHash = previousHash;
+            Data = data;
+            Hash = GenerateHash();
+        }
+
+        public string GenerateHash()
+        {
+            SHA256 sha256 = SHA256.Create();
+
+            byte[] inputBytes = Encoding.ASCII.GetBytes($"{TimeStamp}-{PreviousHash ?? ""}-{Data}-{Nouce}");
+            byte[] outputBytes = sha256.ComputeHash(inputBytes);
+
+            return Convert.ToBase64String(outputBytes);
+        }
+
+        public void Mine(int difficulty)
+        {
+            var leadingZero = new string('0', difficulty);
+
+            while(this.Hash==null || this.Hash.Substring(0,difficulty)!=leadingZero)
+            {
+                this.Nouce++;
+                this.Hash = this.GenerateHash();
+            }
+        }
+    }
+}
